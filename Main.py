dc1f0249997b784e64c29649e48ebd239a026255
fitriayuliskaa <iniipittttt@gmail.com>
2023-10-22 02:41:42 +0700

    Added multiplication and division functions

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from math_function import add
+from Math_function import add
 
 
 def main():
@@ -9,6 +9,7 @@ def main():
 
     if operator == "+":
         result = add(data_1, data_2)
+
 
     print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
